c3f551c3c471ce1db59c279190a39fc0d7a90fda
Tae Hee Choi <dadac76@hotmail.com>
2021-08-18 10:49:09 -0700

    cleanedSearch

--- src/utils/appUtils.js
+++ src/utils/appUtils.js
@@ -1,7 +1,10 @@
 export const cleanedSearchNumbers = (search) => {
-    const regex =  /[.,\s]/g;
-    return search.replace(regex, ', ')
-    
+    const regex = /[.,\s]/g;
+    let cleanedSearch = search.trim().replace(regex, ',')
+    cleanedSearch = cleanedSearch.split(',').sort() // 30, 20 -> 20, 30
+    cleanedSearch = cleanedSearch.map(item => item.padStart(2, '0')).join(', ') // leading 0 and add comma 44, 2 -> 02, 44
+    return cleanedSearch
+
 }
 
 export const localDateTime = (date) => new Date(date).toLocaleDateString();
